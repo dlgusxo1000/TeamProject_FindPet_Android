6f6a58f249fb285f1c57857df9a3bc214f02776f
dlgusxo1000 <dlgusxo88@naver.com>
2022-11-22 20:45:14 +0900

    first commit

---
+++ app/src/main/java/com/teamProject/FindPet_Android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.findpet_android.TeamProject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import com.teamProject.FindPet_Android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/teamProject/FindPet_Android/fragment/FindPassword.kt
@@ -0,0 +1,20 @@
+package com.findpet_android.TeamProject.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.teamProject.FindPet_Android.R
+
+class FindPassword : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_find_password, container, false)
+    }
+}
